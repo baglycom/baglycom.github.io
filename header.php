1f385d556d080cf6ba89a0793cd3c0c583aea34a
baglycom <39011363+baglycom@users.noreply.github.com>
2018-05-05 18:24:26 +0100

    First website upload

---
+++ header.php
@@ -0,0 +1,7 @@
+<DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>UK SVG Map</title>
+	<link rel="stylesheet" type="text/css" href="includes/style.css">
+</head>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include 'header.php'; ?>
+
+<body>
+	<div class="wrapper">
+		<div id="index menu"></div>
+		<div id="index map"> <?php include 'includes/united_kingdom.svg'; ?> </div>		
+		<div></div>
+
+	</div>
+</body>
